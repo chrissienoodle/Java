f1a0cf00e3a867ae1958f21f6a5a6b32147b5f32
Christine Gunsenheimer <chrisgunsen@gmail.com>
2018-11-06 10:24:33 +0100

    Initialisation des constantes et création du plateau

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //déclaration des variables
+        final int LENGTH = (int)(Math.random()*100);
+        final int HEIGHT = (int)(Math.random()*100);
+        final char WALL = '#';
+        final char PATH = ' ';
+        final char START = '>';
+        final char END = 'X';
+        final char PLAYER = 'x';
+        char board[][] = new char[HEIGHT][LENGTH];
+
+    }
+}
